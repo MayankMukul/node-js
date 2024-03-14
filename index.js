166f0f80dc5466d49e262ddf4f666eb07f59bdb0
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-14 23:43:26 +0530

    express added

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const index = fs.readFileSync("index.html",'utf-8');
-const productPage = fs.readFileSync("product.html",'utf-8');
-const data = JSON.parse(fs.readFileSync( "data.json"));
-const products = data.products[0];
+const server = express();
 
-
-const server = http.createServer((req,res)=>{
-
-    switch(req.url){
-        case "/":
-            res.setHeader('Context-Type', 'text/html');
-            res.end(index);
-            break;
-        case "/api":
-            res.setHeader('Content-Type','application/json');
-            res.end(JSON.stringify(data));;
-            break;
-        case "/product":
-            res.setHeader('Context-Type', 'text/html');
-            let newproductPage = productPage
-              .replace("**title**", products.title)
-              .replace("**price**", products.price)
-              .replace("**brand**", products.brand)
-              .replace("**imgurl**", products.thumbnail);
-            res.end(newproductPage);
-            break;
-        default :
-            res.writeHead(404);
-            res.end(`Not Found: `);
-    }
-    console.log(`Request received  `);
+server.get('/',(req,res)=>{
+    res.send("hello");
 })
 
-server.listen(8080);
+server.listen(8080,()=>{
+    console.log("server started");
+})
