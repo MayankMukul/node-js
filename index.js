065911d519c5ac0c62393aef1dfb46d22fcdfa1b
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-15 23:54:53 +0530

    methods

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express = require('express');
 
 const server = express();
 
-server.get('/',(req,res)=>{
-    res.send("hello");
+server.get("/", (req, res) => {
+    res.json({type: "GET"});
+});
+
+server.post('/', (req, res) =>{
+    res.json({type: "POST"});
+});
+
+server.put('/', (req, res) =>{
+    res.json({type: "PUT"});
+});
+
+server.delete('/', (req, res) =>{
+    res.json({type: "DELETE"});
+});
+
+server.patch('/', (req, res) =>{
+    res.json({type: "PATCH"});
+});
+
+server.get('/demo',(req,res)=>{
+    res.send("<h1>hello<h1/>");
 })
+
 
 server.listen(8080,()=>{
     console.log("server started");
