55aafa075660339207e8b90e024729ea7e6ae8bc
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-03-23 23:53:39 +0530

    model view controller

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require("fs");
 const productdata = JSON.parse(fs.readFileSync( "data.json"));
+const productController = require('./controller/product.js');
+
 const server = express();
 //built-in middleware
 server.use(express.json())
@@ -40,15 +42,14 @@ const auth = ((req,res,next)=>{
 //     }
 // })
 
-server.get("/products", (req, res) => {
-    res.json(productdata);
-});
 
-server.get("/products/:id", (req, res) => {
-    const id = +req.params.id;
-    const product = productdata.find(p=> p.id===id);
-    res.json(product);
-});
+
+server.get("/products", productController.getAllProducts);
+server.get("/products/:id", productController.getProduct);
+server.post('/product', productController.addProduct);
+server.put('/products/:id', productController.replaceProduct );
+server.patch('/products/:id', productController.updateProduct);
+server.delete('/products/:id', productController.deleteProduct);
 
 server.get("/",auth, (req, res) => {
     res.json({type: "GET"});
@@ -58,22 +59,7 @@ server.post('/', (req, res) =>{
     res.json({type: "POST"});
 });
 
-server.post('/product', (req, res) =>{
-    // console.log(req.body);
-    productdata.push(req.body);
-    res.json(req.body);
-});
-
 server.put('/', (req, res) =>{
-    res.json({type: "PUT"});
-});
-
-server.put('/products/:id', (req, res) =>{
-    const id = +req.params.id;
-    const productindex = productdata.findIndex(p=> p.id===id);
-
-    productdata.splice(productindex,1,{...req.body, id:id})
-    
     res.json({type: "PUT"});
 });
 
@@ -81,27 +67,10 @@ server.patch('/', (req, res) =>{
     res.json({type: "PATCH"});
 });
 
-server.patch('/products/:id', (req, res) =>{
-    const id = +req.params.id;
-    const productindex = productdata.findIndex(p=> p.id===id);
-    let tempProduct = productdata[productindex];
-    productdata.splice(productindex,1,{...tempProduct,...req.body})
-    
-    res.json({type: "PATCH"});
-});
-
 server.delete('/', (req, res) =>{
     res.json({type: "DELETE"});
 });
 
-server.delete('/products/:id', (req, res) =>{
-    const id = +req.params.id;
-    const productindex = productdata.findIndex(p=> p.id===id);
-    let tempProduct = productdata[productindex];
-    productdata.splice(productindex,1)
-    
-    res.json(tempProduct);
-});
 
 // server.get('/demo',(req,res)=>{
 //     res.send("<h1>hello<h1/>");
