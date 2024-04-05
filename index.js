5922b3fc9059ac97fec2bd5c1d557024aa1174d4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-05 23:26:50 +0530

    mongoose added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/node_ecommerce');
 // const morgan = require('morgan');
 const fs = require("fs");
 // const productdata = JSON.parse(fs.readFileSync( "data.json"));
 const productRouter = require('./routes/product.js')
 const userRouter = require('./routes/user.js')
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/node_ecommerce');
+console.log("Connected to MongoDB successfully!");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 
 const server = express();
