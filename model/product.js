ea0534807129ec835d670db13614f422bf0c7c0f
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-09 23:21:49 +0530

    schema validator

--- model/product.js
+++ model/product.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  title: String, // String is shorthand for {type: String}
-  description: String,
-  price : Number,
-  discountPercentage : Number,
-  rating : Number,
-  brand: String,
-  category : String,
+  title: {type:String, required : true}, // String is shorthand for {type: String}
+  description: {type:String, required : true},
+  price : {type:Number,min : [0, "Wrong Price"], required : true},
+  discountPercentage : {type:Number, min : [0, "Wrong Discount"], max : [50, "Wrong Max Discount"]},
+  rating : {type:Number, min : [0, "Wrong  Rating"], max : [5]},
+  brand: {type:String, required : true},
+  category : {type:String, required : true},
+  thumbnail: {type : String , required : true},
   images : [String],
 });
 
