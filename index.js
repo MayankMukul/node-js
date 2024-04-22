a192d339727e89cea45195899c66d73911ac1fef
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-04-22 23:51:43 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/node_ecommerce');
-// const morgan = require('morgan');
-const fs = require("fs");
-// const productdata = JSON.parse(fs.readFileSync( "data.json"));
+const cors = require('cors');
+const path = require('path');
+
+// OA0zkU855YSApQcD
+//password
 const productRouter = require('./routes/product.js')
 const userRouter = require('./routes/user.js')
 
@@ -13,22 +14,23 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/node_ecommerce');
 console.log("Connected to MongoDB successfully!");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
+  }
 
 
 const server = express();
 //built-in middleware
+server.use(cors());
 server.use(express.json())
 
+server.use(express.static(path.resolve(__dirname,'build')));
 server.use('/products',productRouter.router);
 server.use('/users',userRouter.router);
+server.use('*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build', 'index.html'));
+});
 
-//body parser
-// server.use(morgan('default'));
 
 //static hoisting
-server.use(express.static('public'));
 
 server.use((req,res,next)=>{
     // console.log(req.method, req.ip, new Date())
@@ -37,16 +39,6 @@ server.use((req,res,next)=>{
 
 
 
-
-
-
-// Model-View-Controller (MVC)
-
-
-
-// server.get('/demo',(req,res)=>{
-//     res.send("<h1>hello<h1/>");
-// })
 
 
 server.listen(8080,()=>{
