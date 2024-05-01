b9c6560e12dbe86ba3bf92dd975c51b30ecf9e8d
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-05-01 23:22:00 +0530

    update

--- model/product.js
+++ model/product.js
@@ -6,7 +6,7 @@ const productSchema = new Schema({
   description: {type:String, required : true},
   price : {type:Number,min : [0, "Wrong Price"], required : true},
   discountPercentage : {type:Number, min : [0, "Wrong Discount"], max : [50, "Wrong Max Discount"]},
-  rating : {type:Number, min : [0, "Wrong  Rating"], max : [5]},
+  rating : {type:Number, min : [0, "Wrong  Rating"], max : [5], default: 0 },
   brand: {type:String, required : true},
   category : {type:String, required : true},
   thumbnail: {type : String , required : true},
