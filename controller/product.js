c2ccd0c1b4b3e8597e7a9c9c66b991b39f2ff7b4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-05-02 23:36:02 +0530

    deploy

--- controller/product.js
+++ controller/product.js
@@ -1,6 +1,7 @@
 
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync( "data.json"));
+const path = require('path');
+const data = JSON.parse(fs.readFileSync( path.resolve(__dirname,"data.json")));
 const productdata = data.products;
 // console.log(productdata)
 
