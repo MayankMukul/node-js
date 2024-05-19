f1ce745392608c15a30bb657349765d9f1bdfea3
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-05-19 23:54:04 +0530

    jwt added

--- controller/user.js
+++ controller/user.js
@@ -5,6 +5,8 @@ const path = require('path');
 const model = require('../model/user');
 const User = model.User;
 
+const jwt = require('jsonwebtoken');
+
 // const data = JSON.parse(fs.readFileSync(  path.resolve(__dirname,"../data.json")));
 // const userdata = data.users;
 // console.log(productdata)
@@ -23,6 +25,8 @@ exports.getUser = async (req, res) => {
 exports.createUser = async (req, res) => {
 
   const user = new User(req.body);
+  let token = jwt.sign({email:req.body.email}, 'shhhhh')
+  user.token = token;
   await user.save().then(doc=>{
     console.log(doc)
     res.status(200).json(doc)
