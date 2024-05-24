06bd2f85dd4d8a18eb4eec4b38314bef435eb2b2
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-05-24 15:36:21 +0530

    auth added

--- controller/user.js
+++ controller/user.js
@@ -5,7 +5,6 @@ const path = require('path');
 const model = require('../model/user');
 const User = model.User;
 
-const jwt = require('jsonwebtoken');
 
 // const data = JSON.parse(fs.readFileSync(  path.resolve(__dirname,"../data.json")));
 // const userdata = data.users;
@@ -22,19 +21,7 @@ exports.getUser = async (req, res) => {
   res.json(user);
 };
 
-exports.createUser = async (req, res) => {
 
-  const user = new User(req.body);
-  let token = jwt.sign({email:req.body.email}, 'shhhhh')
-  user.token = token;
-  await user.save().then(doc=>{
-    console.log(doc)
-    res.status(200).json(doc)
-  }).catch(err=>{
-    res.status(400).json(err);
-  })
-  
-};
 
 exports.replaceUser = async (req, res) => {
   const id = +req.params.id;
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router
   .get("/", userController.getAllUsers)
   .get("/:id", userController.getUser)
-  .post("/", userController.createUser)
+  
   .put("/:id", userController.replaceUser)
   .patch("/:id", userController.updateUser)
   .delete("/:id", userController.deleteUser)
