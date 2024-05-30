921e68df1c3b51a5e950f4481fa436644c676af7
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-05-30 23:10:04 +0530

    login added

--- controller/auth.js
+++ controller/auth.js
@@ -5,10 +5,14 @@ const model = require('../model/user');
 const User = model.User;
 const path = require('path')
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 const privatekey = fs.readFileSync(path.resolve(__dirname,'../private.key'),'utf-8');
 
-exports.createUser = async (req, res) => {
+exports.signUp = async (req, res) => {
     const user = new User(req.body);
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    user.password = hash;
+
     let token = jwt.sign({email:req.body.email}, privatekey, {algorithm:'RS256'})
     user.token = token;
     await user.save().then(doc=>{
@@ -19,3 +23,26 @@ exports.createUser = async (req, res) => {
     })
     
   };
+
+  exports.login = async (req, res)=>{
+    try {
+      const doc = await User.findOne({ email: req.body.email });
+      const isAuth = bcrypt.compareSync(req.body.password, doc.password);
+      if (isAuth) {
+        let token = jwt.sign({ email: req.body.email }, privatekey, {
+          algorithm: "RS256",
+        });
+        doc.token = token;
+        doc.save().then(()=>{
+          res.status(200).json({token})
+        }).catch(err=>{
+          res.status(400).json(err)
+        })
+        
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (err) {
+      res.status(401).json(err);
+    }
+  }
