880e28131f60ce1c962c3c179892c3c385223eb4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2024-06-07 23:53:02 +0530

    send data using buffer

--- events.js
+++ events.js
@@ -1,11 +1,24 @@
 const EventEmitter = require('events');
-
 const em = new EventEmitter();
 
-em.on('demo',()=>{
-    console.log('demo')
+const fs = require('fs');
+const rr = fs.createReadStream(''); // file path
+
+rr.on('data',(data)=>{
+    console.log(data);
+})
+
+rr.on('end',(data)=>{
+    console.log(data);
+})
+
+
+
+//events
+em.on('demo',(data)=>{
+    console.log('demo',data)
 })
 
 setTimeout(()=>{
-    em.emit('demo');
+    em.emit('demo',{name:'dummy'}); //object can also be passed
 },4000);
